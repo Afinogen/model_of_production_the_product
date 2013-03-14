0fef515529871f872292a1d3813ab49210720290
Pozdnyakov Sergey <afinogen89@gmail.com>
2013-03-14 23:25:02 +0400

    0.0.1.2-a

---
+++ main.cpp
@@ -0,0 +1,64 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 14.03.2013
+ *      Author: Afinogen
+ */
+#include <windows.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+//Функция таймера
+VOID CALLBACK TimerProc(HWND hWnd, UINT nMsg, UINT nIDEvent, DWORD dwTime)
+{
+
+}
+
+//Главная функция
+int WINAPI WinMain (HINSTANCE hThisInstance,HINSTANCE hPrevInstance,LPSTR lpszArgument,int nFunsterStil)
+{
+    MSG messages;            /* Here messages to the application are saved */
+    /* Run the message loop. It will run until GetMessage() returns 0 */
+    while (GetMessage (&messages, NULL, 0, 0))
+    {
+        /* Translate virtual-key messages into character messages */
+        TranslateMessage(&messages);
+        /* Send message to WindowProcedure */
+        DispatchMessage(&messages);
+    }
+
+    /* The program return-value is 0 - The value that PostQuitMessage() gave */
+    return messages.wParam;
+}
+
+LRESULT CALLBACK  WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+ 	switch (message)                  /* handle the messages */
+    {
+    	case WM_PAINT:
+    		hdc = BeginPaint(hwnd, &ps);
+
+    		EndPaint(hwnd, &ps);
+        break;
+    	case WM_COMMAND:
+    	break;
+    	case WM_CLOSE:
+
+    		PostQuitMessage (0);
+    	break;
+    	case WM_DESTROY:
+
+            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+            break;
+        default:                      /* for messages that we don't deal with */
+            return DefWindowProc (hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
